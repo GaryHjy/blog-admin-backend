f312a935917d07844c8da9e6be4a9dc3dee3bea3
GaryHjy <782316959@qq.com>
2020-03-21 16:27:23 +0800

    feat: 添加环境变量配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,45 @@
+import * as path from 'path';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import databaseConfig from './config/database.config';
+
+// const entitiesPath =
+//   process.env.NODE_ENV === 'production'
+//     ? path.resolve('./**/*.entity.js')
+//     : path.resolve('./**/*.entity.ts');
+
+
+const entitiesPath = path.resolve(__dirname + '/*/entities/*.entity{.ts,.js}')
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    // 配置加载配置文件
+    ConfigModule.forRoot({
+      load: [databaseConfig] // 引入配置文件
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], // 要import, 不然会报错
+      // 接收到configService
+      useFactory: async (config: ConfigService) => ({
+        type: config.get('database.type'),
+        host: config.get('database.host'),
+        port: config.get('database.port'),
+        username: config.get('database.username'),
+        password: config.get('database.password'),
+        database: config.get('database.database'),
+        entities: [entitiesPath],
+        synchronize: config.get('database.synchronize'),
+      }),
+      inject: [ConfigService], // 注入
+    }),
+    UserModule,
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

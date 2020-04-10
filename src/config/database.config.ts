6c61f2c0f261a9a3317d7d5cf669f8d6518c9fa5
GaryHjy <782316959@qq.com>
2020-04-10 11:38:45 +0800

    feat: 修改数据库配置文件

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,11 +2,11 @@ import { registerAs } from '@nestjs/config';
 
 // 数据库配置
 export default registerAs('database', () => ({
-  type: process.env.DB_TYPE,
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  synchronize: false,
+  type: process.env.DB_TYPE, // 数据库类型
+  host: process.env.DB_HOST, // 数据库host
+  port: Number(process.env.DB_PORT), // 数据库端口
+  username: process.env.DB_USERNAME, // 用户名
+  password: process.env.DB_PASSWORD, // 密码
+  database: process.env.DB_DATABASE, // 数据库
+  synchronize: true, // 每次应用启动后自动对数据库的视图Schema进行自动Migration
 }))

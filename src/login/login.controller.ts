15f46d57eb74a2d8123b84432a6fcda6a68ab9e9
GaryHjy <782316959@qq.com>
2020-04-15 22:52:23 +0800

    feat: 修改登录接口返回dto

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiOkResponse } from '@nestjs/swagger';
 import { UserLoginDto } from '../user/dto/user.login.dto';
-import { UserRep } from '../user/dto/user.rep.dto';
 import { UserService } from '../user/user.service';
 import { AuthService } from 'src/auth/auth.service';
+import { UserLoginRep } from '../user/dto/user.login.rep.dto';
 
 @Controller('login')
 @ApiTags('用户登录')
@@ -16,11 +16,11 @@ export class LoginController {
 
   @Post()
   @ApiOperation({ summary: '用户登录', description: '根据用户名密码登录'})
-  @ApiOkResponse({ type: UserRep})
-  async login(@Body() userLoginDto: UserLoginDto): Promise<UserRep> {
+  @ApiOkResponse({ type: UserLoginRep})
+  async login(@Body() userLoginDto: UserLoginDto): Promise<UserLoginRep> {
     const user = await this.userService.login(userLoginDto);
     const accessToken = await this.authService.sign(user);
-    
+
     return {
       ...user,
       ...accessToken

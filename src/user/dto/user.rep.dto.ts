9be204372ebd07680b3b9fc847ba1eb712216068
GaryHjy <782316959@qq.com>
2020-04-15 22:58:33 +0800

    feat: 更新UserRepDto

--- src/user/dto/user.rep.dto.ts
+++ src/user/dto/user.rep.dto.ts
@@ -8,16 +8,22 @@ export class UserRep {
   @ApiProperty({ description: '用户名' })
   username: string;
 
-  @ApiProperty({ description: '用户状态' })
+  @ApiProperty({ 
+    description: '用户状态',
+    enum: [0, 1]
+   })
   status: number
+
+  @ApiProperty({ 
+    description: '用户权限',
+    enum: ['root', 'admin', 'guest']
+  })
+  role: string;
 
   @ApiProperty({ description: '创建时间' })
   createdAt: Date;
 
   @ApiProperty({ description: '更新时间' })
   updatedAt: Date;
-
-  @ApiProperty({ description: '用户权限' })
-  role: string;
 
 }

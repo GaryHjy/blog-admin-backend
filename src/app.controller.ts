2515614e1e71188c1318792fe923ec13c9a080da
GaryHjy <782316959@qq.com>
2020-05-28 20:10:32 +0800

    feat: app.controller添加登录接口

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Post, Body } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiOkResponse } from '@nestjs/swagger';
 import { UserService } from './user/user.service';
 import { AuthService } from './auth/auth.service';
 import { UserLoginRep } from './user/dto/user.login.rep.dto';
+import { UserLoginDto } from './user/dto/user.login.dto';
 
 @Controller()
 @ApiTags('全局')
@@ -21,6 +22,19 @@ export class AppController {
     return {
       ...user,
       accessToken: token
+    }
+  }
+
+  @Post('login')
+  @ApiOperation({ summary: '用户登录', description: '根据用户名密码登录' })
+  @ApiOkResponse({ type: UserLoginRep })
+  async login(@Body() userLoginDto: UserLoginDto): Promise<UserLoginRep> {
+    const user = await this.userService.login(userLoginDto);
+    const { accessToken } = await this.authService.sign(user);
+
+    return {
+      ...user,
+      accessToken
     }
   }
 }

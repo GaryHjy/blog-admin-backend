23c1296353d88715a88cd96fb2e09f81c938a8be
GaryHjy <782316959@qq.com>
2020-05-31 21:53:36 +0800

    feat: 添加user_role.entity

---
+++ src/entities/user_role.entity.ts
@@ -0,0 +1,41 @@
+import { Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, Column } from "typeorm";
+
+@Entity()
+export class UserRole {
+  @PrimaryGeneratedColumn({
+    type: 'int',
+    name: 'id',
+    comment: '主键id',
+  })
+  id: number;
+
+  @Column('int', {
+    nullable: true,
+    name: 'user_id',
+    comment: '用户id'
+  })
+  userId: number;
+
+  @Column('int', {
+    nullable: true,
+    name: 'role_id',
+    comment: '角色id'
+  })
+  roleId: number;
+
+  @CreateDateColumn({
+    nullable: false,
+    type: 'timestamp',
+    name: 'created_time',
+    comment: '创建时间',
+  })
+  createdTime: Date;
+
+  @UpdateDateColumn({
+    nullable: false,
+    type: 'timestamp',
+    name: 'updated_time',
+    comment: '更新时间'
+  })
+  updatedTime: Date;
+}

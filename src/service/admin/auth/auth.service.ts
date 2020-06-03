0188d12e6c8c5457faedb5b861b1ddc0d97cbc06
GaryHjy <782316959@qq.com>
2020-06-04 01:31:50 +0800

    feat: 添加auth.service

---
+++ src/service/admin/auth/auth.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt'
+import { checkPwd } from 'src/utils/encrypt';
+import { UserService } from 'src/service/admin/user/user.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService
+  ) {}
+
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.userService.findByUsername(username)
+    if (user && checkPwd(password, user.password)) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async sign(user: any) {
+    const {username, password, id } = user;
+    return {
+      accessToken: this.jwtService.sign({
+        id,
+        username,
+        password,
+      })
+    }
+  }
+
+  // 校验token是否有效
+  async verifyToken(token: string) {
+    try {
+      return await this.jwtService.verify(token)
+    } catch (e) {
+      throw new HttpException('token已过期，请重新登录', HttpStatus.UNAUTHORIZED); // 捕获过期异常
+    }
+  }
+}

4fc6693ba498ab9cac93237af64295428f5fc37c
GaryHjy <782316959@qq.com>
2020-06-04 01:36:26 +0800

    feat: 删除旧文件user

--- src/user/dto/user.login.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger"
-
-export class UserLoginDto {
-  @ApiProperty({ description: '用户名' })
-  username: string
-
-  @ApiProperty({ description: '密码' })
-  password: string
-}
--- src/user/dto/user.login.rep.dto.ts
+++
@@ -1,9 +0,0 @@
-
-import { UserRep } from './user.rep.dto';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class UserLoginRep extends UserRep {
-
-  @ApiProperty({ description: '用户token'})
-  accessToken?: string;
-}

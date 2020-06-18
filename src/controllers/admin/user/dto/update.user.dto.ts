36616279a0e0960f1a8d4264473c9e6955791e06
GaryHjy <782316959@qq.com>
2020-06-18 18:59:15 +0800

    feat: 修改update.user.dto

--- src/controllers/admin/user/dto/update.user.dto.ts
+++ src/controllers/admin/user/dto/update.user.dto.ts
@@ -1,3 +1,10 @@
 import { UserDto } from './user.dto';
+import { IsOptional } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
-export class UpdateUserDto extends UserDto {}
+export class UpdateUserDto extends UserDto {
+
+  @ApiPropertyOptional({ required: false, description: '用户名' })
+  @IsOptional()
+  readonly username?: string;
+}

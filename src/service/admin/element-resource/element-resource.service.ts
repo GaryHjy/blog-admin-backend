54e52e3d1cec296856575552b99eeff025dd7cbb
GaryHjy <782316959@qq.com>
2020-06-29 21:56:41 +0800

    feat: 引入ElementResourceRepository

--- src/service/admin/element-resource/element-resource.service.ts
+++ src/service/admin/element-resource/element-resource.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ElementResource } from 'src/entities/element_resource.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ElementResourceService {}
+export class ElementResourceService {
+  constructor(
+    @InjectRepository(ElementResource)
+    private readonly ElementResourceRepository: Repository<ElementResource>
+  ) {}
+}
